e1888274b886e8bd17cf7b664466e850b811fc68
Askeladd42 <piema.lam@gmail.com>
2021-04-05 15:11:23 +0200

    free the stack function added

--- Makefile
+++ Makefile
@@ -6,12 +6,14 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/15 11:29:08 by plam              #+#    #+#              #
-#    Updated: 2021/03/18 17:14:48 by plam             ###   ########.fr        #
+#    Updated: 2021/04/05 14:31:24 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC			=	checker.c\
-				push_swap.c
+				push_swap.c\
+				free_stack.c\
+				check_info.c
 
 NAME		=	push_swap
 
@@ -38,4 +40,4 @@ fclean:		clean
 
 re:			fclean all
 
-.PHONY:		all clean fclean re bonus
+.PHONY:		all clean fclean re bonus checker push_swap
--- checker.c
+++ checker.c
@@ -6,65 +6,30 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 11:29:27 by plam              #+#    #+#             */
-/*   Updated: 2021/03/24 08:22:05 by plam             ###   ########.fr       */
+/*   Updated: 2021/04/05 15:10:20 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 
-t_frame	*stack_init(t_stack pos)
-{
-	t_frame	*frame;
-
-	frame = malloc(sizeof(t_frame));
-	if (frame == NULL)
-		return (NULL);
-	frame->next = NULL;
-	frame->stack = pos;
-	return (frame);
-}
-
-int	stack_push(t_frame **item, t_stack stack)
-{
-	t_frame	*old;
-
-	old = *item;
-	*item = stack_init(stack);
-	if (*item == NULL)
-		return (ERR);
-	(*item)->next = old;
-	return (OK);
-}
-
-t_stack	stack_pop(t_frame **item)
-{
-	t_stack	stack;
-	t_frame	*old;
-
-	if (*item == NULL)
-		return ((t_stack){INT_MAX, INT_MIN});
-	old = *item;
-	*item = (*item)->next;
-	stack = old->stack;
-	free(old);
-	return (stack);
-}
-
-int	stack_len(t_frame *item)
-{
-	int	i;
-
-	i = 0;
-	while (item != NULL)
-	{
-		item = item->next;
-		i++;
-	}
-	return (i);
-}
-
-
 int	checker(int	*data)
 {
 	
+}
+
+void	check_sort(char *s_type)
+{
+	printf("OK\n");
+	printf("KO\n");
+}
+int	main(int ac, char **av)
+{
+	if (ac > 1 && av[1])
+	{
+		check_sort(stdin);
+		checker(av[1]);
+	}
+	else
+		printf("Error\n");
+	return (0);
 }
