a01ea8c4983ef28030508da4e3ec4a8bb60b2b11
Pierre-marie LAM <plam@e3r6p7.42.fr>
2021-10-07 15:02:03 +0200

    stack tests

--- srcs/stk_check.c
+++ srcs/stk_check.c
@@ -6,15 +6,21 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 12:05:23 by plam              #+#    #+#             */
-/*   Updated: 2021/10/07 13:54:43 by plam             ###   ########.fr       */
+/*   Updated: 2021/10/07 15:02:00 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 #include "instructions.h"
 
-int	dup_check(char **str)
+int	dup_check(char **str, t_lst *l)
 {
+	int	n;
+
+	n = ft_atoi(*str);
+	l->data = n;
+	ft_lstadd_front(l,...);
+
 	return (TRUE);
 }
 
