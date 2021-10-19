9eb9c622124904da3097731a2b22dde75b7b8e27
Askeladd42 <piema.lam@gmail.com>
2021-10-19 10:25:03 +0200

    basic instructions added

--- srcs/s.c
+++ srcs/s.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 15:01:35 by plam              #+#    #+#             */
-/*   Updated: 2021/10/18 15:08:44 by plam             ###   ########.fr       */
+/*   Updated: 2021/10/19 10:23:42 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,13 @@
 void	sa(t_stk *a)
 {
 	if (a-> size > 1)
-	{
 		swap_int(&a->first->data, &a->first->next->data);
-		printf("%s\n", "sa");
-	}
 }
 
 void	sb(t_stk *b)
 {
 	if (b-> size > 1)
-	{
 		swap_int(&b->first->data, &b->first->next->data);
-		printf("%s\n", "sb");
-	}
 }
 
 void	ss(t_stk *a, t_stk *b)
