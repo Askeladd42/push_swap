a36fd8c8684b6794f7a72103b534caa88a738d3c
Pierre-marie LAM <plam@e3r7p5.42.fr>
2021-10-19 15:25:41 +0200

    adding checking info instructions

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 14:28:50 by plam              #+#    #+#             */
-/*   Updated: 2021/10/19 15:18:11 by plam             ###   ########.fr       */
+/*   Updated: 2021/10/19 15:25:24 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,11 +79,11 @@ int	main(int ac, char **av)
 			exit_error_push_swap();
 		else
 		{
+			create_lst();										// to complete with the number lists
+			get_all_values_to_lst(av, check_info(av[1]));
 			a = malloc_stk(NULL, NULL, check_info(av[1]));
 			b = malloc_stk(NULL, NULL, check_info(av[1]));
 		}
-		create_lst();										// to complete with the number lists
-		get_all_values_to_lst(av, check_info(av[1]));
 	}
 	return (0);
 }
