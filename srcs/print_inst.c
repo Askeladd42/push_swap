f2f1e39648ee5e907d81e578bd87688d6262dcb2
Askeladd42 <piema.lam@gmail.com>
2021-10-21 13:51:24 +0200

    basic instructions added + print functions started

---
+++ srcs/print_inst.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_inst.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 13:44:06 by plam              #+#    #+#             */
+/*   Updated: 2021/10/21 13:51:04 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stack.h"
+#include "instructions.h"
+
+/*
+
+Make the different print for each instructions or a general function ?
+Maybe looking at a list ?
+print first, then execute the commands
+
+*/
