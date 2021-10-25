87ac3f1ad61bca45b491d71a2605c9b3cf4f04d5
Askeladd42 <piema.lam@gmail.com>
2021-10-25 16:16:16 +0200

    basic instructions added + print functions started

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 14:43:53 by plam              #+#    #+#             */
-/*   Updated: 2021/10/19 14:40:49 by plam             ###   ########.fr       */
+/*   Updated: 2021/10/25 16:15:59 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 # define PUSH_SWAP_H
 
 # include <unistd.h>
-#include "instructions.h"
+# include "libft.h"
+# include "stack.h"
+# include "instructions.h"
 
+int	check_order(t_lst *l);
 int	check_info(char *arg);
 
 #endif
