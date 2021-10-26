dd7fd218bf1991d68a54a964072a2940e8e977b3
Askeladd42 <piema.lam@gmail.com>
2021-10-26 12:07:16 +0200

    basic instructions added + print functions started

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,24 @@
 #include "stack.h"
 #include "instructions.h"
 
-int main()
+int	main(int argc, char *argv[])
 {
-	t_stk	stk;
-	char *l = "1, 3, 2";
+	t_stack		*a;
+	t_stack		*b;
 
-	*stk = malloc_stk();
-	free_stk(*stk);
-	return(0);
+	if (argc < 2)
+		return (0);
+	a = generate_stack_a_content(argv, argc);
+	if (!a)
+		return (-1);
+	b = malloc_stack(NULL, NULL, 0);
+	if (!b)
+	{
+		free_stack(a);
+		return (-1);
+	}
+	select_algo(a, b, a->size);
+	free_stack(a);
+	free_stack(b);
+	return (0);
 }
