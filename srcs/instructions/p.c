3e894a5768b6ea0c09fa3ee1709a317c760cef86
Askeladd42 <piema.lam@gmail.com>
2021-11-26 15:31:23 +0100

    working on the tab checking before using the stack

--- srcs/instructions/p.c
+++ srcs/instructions/p.c
@@ -6,11 +6,11 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 15:02:25 by plam              #+#    #+#             */
-/*   Updated: 2021/11/26 15:31:07 by plam             ###   ########.fr       */
+/*   Updated: 2021/11/26 15:31:19 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stack.h"
+#include "struct.h"
 #include "instructions.h"
 
 void	pa(t_stk *stk)
