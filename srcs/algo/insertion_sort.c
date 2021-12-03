213a2996c43b3d8d84f4754ea51e2f8099860115
Pierre-marie LAM <plam@e2r5p18.clusters.42paris.fr>
2021-12-03 13:03:09 +0100

    added insertion sort method algorithm && low nb sort algorithms

--- srcs/algo/insertion_sort.c
+++ srcs/algo/insertion_sort.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 12:52:54 by plam              #+#    #+#             */
-/*   Updated: 2021/12/03 12:53:30 by plam             ###   ########.fr       */
+/*   Updated: 2021/12/03 13:03:04 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 void	insertion_sort(t_tab *tab)
 {
-	
+	int	i;
+	int	j;
+	int	x;
+
+	i = 0;
+	while (i < tab->size - 1)
+	{
+		if (tab->sort[i + 1] < tab->sort[i])
+		{
+			j = i;
+			x = tab->sort[j + 1];
+			while (j > -1 && x < tab->sort[j])
+			{
+				tab->sort[j + 1] = tab->sort[j];
+				j--;
+			}
+			tab->sort[j + 1] = x;
+		}
+		i++;
+	}
 }
