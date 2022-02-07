8d1980d7425783fd6970d38ea23f11778a7f9043
Pierre-marie LAM <plam@e2r10p10.clusters.42paris.fr>
2022-02-07 14:11:07 +0100

    gnl normed

--- srcs/gnl/get_next_line.c
+++ srcs/gnl/get_next_line.c
@@ -6,20 +6,26 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 10:07:36 by plam              #+#    #+#             */
-/*   Updated: 2021/12/30 12:41:22 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/07 14:10:08 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	init_line(int fd, char **line)
+{
+	*line = NULL;
+	if (!line || fd < 0)
+		return (-1);
+	return (0);
+}
 
 int	get_next_line(int fd, char **line)
 {
 	static t_buff	lst;
 	int				err;
 
-	*line = NULL;
-	if (!line || fd < 0)
-		return (-1);
+	init_line(fd, line);
 	if (lst.buff[0] != '\0')
 	{
 		*line = ft_strcjoin(*line, lst.buff);
